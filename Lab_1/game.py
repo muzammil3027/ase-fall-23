b41ec4e75d907306706d538a5e8f69879c0e6a88
muzammil <m.muzammil@studenti.unipi.it>
2023-11-09 21:58:51 +0100

    update process move

--- Lab_1/game.py
+++ Lab_1/game.py
@@ -62,10 +62,14 @@ class Game:
         """Process the current move and check if it's a win."""
         row, col = move.row, move.col
         self._current_moves[row][col] = move
-        # TODO: check whether the current move leads to a winning combo.
-        # Do not return any values but set variables  self._has_winner 
-        # and self.winner_combo in case of winning combo.
-        # Hint: you can scan pre-computed winning combos in self._winning_combos
+        for combo in self._winning_combos:
+            results = set(self._current_moves[n][m].label for n, m in combo)
+            is_win = (len(results) == 1) and ("" not in results)
+            if is_win:
+                self._has_winner = True
+                self.winner_combo = combo
+                break
+
 
 
     def has_winner(self):
